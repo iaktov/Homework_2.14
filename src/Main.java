d3203ead1f7675e3ccb29782b18373bc14890815
aktov <aktovi@gmail.com>
2022-07-15 22:44:24 +0700

    homework_2.14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Service.StringListImpl;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringListImpl stringList = new StringListImpl(10);
+        stringList.add(0, "hello");
+        stringList.remove(0);
+        System.out.println(stringList.get(0));
+
     }
 }
